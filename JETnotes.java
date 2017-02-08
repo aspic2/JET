fdd2e1043dd243fce0ec1fea572c3627ea16f160
Thomp <thomp@protonmail.com>
2017-02-08 14:14:25 -0600

    first commit

---
+++ JETnotes.java
@@ -0,0 +1,51 @@
+/* JET notes
+
+------------------------------
+1.1 - The History of Java
+
+Java created in 1991
+new numbering system started in 2004
+    generics were added in 2004 for Java J2SE 5.0 (1.5)
+
+Java was purchased by Oracle in 2010
+
+Java SE 8 was released in 2014
+
+Android development only supports Java SE 6 and SE 7
+
+1.2 - Principles and components of Java
+
+Principles of Java
+    -Simple, object-oriented, and familiar
+    -Robust and secure
+    -Architecture-neutral and portable
+    -High-performance
+    -Interpreted, threaded, and dynamic
+
+Java is a consistent language
+
+Understand three OO concepts to understand Java project design:
+    encapsulation,
+    inheritance, and
+    polymorphism
+
+Java is compiled to bytecode, not machinecode.
+    This means that any system with a JVM can read a compiled Java program
+
+Java Development Kit (JDK) contains four tools for compiling and packaging Java:
+    java: runtime
+    javac: compiler
+    javadoc: docs builder
+    jar: archive builder
+
+    These tools are important to know if you run and compile java code from the
+    command line
+
+
+
+
+
+
+
+
+*/
